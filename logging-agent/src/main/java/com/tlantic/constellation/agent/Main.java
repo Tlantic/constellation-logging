4321205ee3035702f4e2a10ed31ca735622b3921
vinnylinck <vinnylinck@outlook.com>
2015-01-22 18:10:32 -0200

    Log4j added as dependency

--- logging-agent/src/main/java/com/tlantic/constellation/agent/Main.java
+++ logging-agent/src/main/java/com/tlantic/constellation/agent/Main.java
@@ -1,7 +1,15 @@
 package com.tlantic.constellation.agent;
 
-public class Main {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+public class Main {
+	
+	static Logger log = LogManager.getLogger( Main.class.getName() );
+	
 	public static void main(String[] args) {
+		
+		
+		
 	}
 }
