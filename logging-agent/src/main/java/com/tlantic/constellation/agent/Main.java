3101b50cff25d3aaac3d90ff4d620fdd71594752
vinnylinck <vinnylinck@outlook.com>
2015-01-27 10:10:03 -0200

    closes #8

--- logging-agent/src/main/java/com/tlantic/constellation/agent/Main.java
+++ logging-agent/src/main/java/com/tlantic/constellation/agent/Main.java
@@ -9,5 +9,16 @@ public class Main {
 	
 	public static void main(String[] args) {
 		log.info("Testando 1... 2... 3...");
+		log.debug("Debugging 1,2,3");
+		log.trace("Tracing 1,2,3");
+		log.warn("Warning 1,2,3");
+		log.error("Ihhhhhhhh....");
+		
+		try {
+			throw new Exception("blowing up!");
+		} catch (Exception e) {
+			log.catching(e);
+		}
+		
 	}
 }
