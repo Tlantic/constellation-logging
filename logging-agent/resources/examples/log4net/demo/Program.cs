7b25cba2ea0d435ee541ec7cb2998654c210a64a
vinnylinck <vinnylinck@outlook.com>
2015-02-05 10:55:01 -0200

    It closes #9. The agent won't send log to shipper, just handle the service. Even this way, log4net and log4j2 were tested to replace any extra lib for logging.

---
+++ logging-agent/resources/examples/log4net/demo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using log4net;
+
+namespace demo
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			ILog log = LogManager.GetLogger ("my-logger-name");
+			log.Info ("yada");
+		}
+	}
+}
--- logging-agent/src/main/java/com/tlantic/constellation/agent/Main.java
+++ logging-agent/src/main/java/com/tlantic/constellation/agent/Main.java
@@ -1,25 +1,7 @@
 package com.tlantic.constellation.agent;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public class Main {
 	
-	static Logger log = LogManager.getLogger( Main.class.getName() );
-	
 	public static void main(String[] args) {
-		log.info("Testando 1... 2... 3...");
-		log.debug("Debugging 1,2,3");
-		log.warn("Warning 1,2,3");
-		log.error("Ihhhhhhhh....");
-		log.fatal("Fuuuuuu....");
-		log.trace("Tracing 1,2,3");
-		
-		try {
-			throw new Exception("blowing up!");
-		} catch (Exception e) {
-			log.catching(e);
-		}
-		
 	}
 }
