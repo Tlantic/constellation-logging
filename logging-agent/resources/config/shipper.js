08a9eacdfdec7bbd4dfe414827fd25494ba43983
vinnylinck <vinnylinck@outlook.com>
2015-02-09 13:36:29 -0200

    Almost finishing #1. Now, log4j2 uses udp to remove network dependency when using the applciation.

--- logging-agent/resources/config/shipper.js
+++ logging-agent/resources/config/shipper.js
@@ -1,15 +1,23 @@
 input {
-	tcp {
+	
+	# Log4j2 input format expected. JSON Layout is not being used because log4j2 message arrives "splitted".
+	# TCP protocol is the default, but is not being used to avoid network dependency when logging.
+	# This channel is used by server => 5.x and other java solutions.
+	# log4j input doesn't work for v2 logging lib.
+	udp {
 		type => "log4j2"
-		host => "localhost"
-		mode => "server"
 		port => "9500"
-		codec => json {}
+		codec => plain {
+			charset => "UTF-8"
+		}
 	}
 
+	# This channel is available for .Net applications configured to use log4net.
+	# Log4net does not provide SocketAppender as log4j, making this solution incompatible with tcp channel.
+	# To support 4.x servers, an udp port channel was created. 
 	udp {
 		type => "log4net"
-		port => "9500"
+		port => "9501"
 		codec => plain {
 			charset => "UTF-8"
 		}
@@ -19,16 +27,33 @@ input {
 
 filter {
 	
-	if [type] == "log4net" {
+	if [type] == "log4j2" {
+			
+			# Log4j2  handles thread and widget differently;
 			grok {
-				match => ["message","%{LOGLEVEL:level} %{TIMESTAMP_ISO8601:sourceTimestamp} %{WORD:thread} %{WORD:widget} - %{GREEDYDATA:message}"]
+				match => ["message","%{IPORHOST:hostname} %{LOGLEVEL:level} %{TIMESTAMP_ISO8601:sourceTimestamp} %{DATA:thread} %{DATA:widget} - %{GREEDYDATA:message}"]
 				overwrite => ["message"]
 			}
+
+			
+	} else if [type] == "log4net" {
+			
+			# Log4net message "normalization": receives a text message and performs a "sushi" to build a json message		
+			grok {
+				match => ["message","%{IPORHOST:hostname} %{LOGLEVEL:level} %{TIMESTAMP_ISO8601:sourceTimestamp} %{WORD:thread} %{WORD:widget} - %{GREEDYDATA:message}"]
+				overwrite => ["message"]
+			}
+	}
+	
+	
+	# NO ONE CARES ABOUT DEBUG MESSAGES
+	if [level] == "DEBUG" {
+			drop {}
 	}
 }
 
 output {
 	stdout {
-		codec => json {}
+		codec => json{}
 	} 
 }
